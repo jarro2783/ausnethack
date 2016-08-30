3629803d8d62f223ee73910e593dc226d125f0aa
Jarryd Beck <jarro.2783@gmail.com>
2016-08-31 08:01:51 +1000

    test zscore

--- site/test.py
+++ site/test.py
@@ -1,6 +1,14 @@
 import unittest
+import wwwnethack
+
+class TestZScore(unittest.TestCase):
+    def test_zscore(self):
+        self.assertEqual(wwwnethack.calculate_z(1), 1.0)
+        self.assertEqual(wwwnethack.calculate_z(2), 1.5)
+        self.assertEqual(wwwnethack.calculate_z(3), 1.75)
 
 def run_tests():
+    unittest.main()
     
 
 if __name__ == '__main__':
