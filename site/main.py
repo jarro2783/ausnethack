d1c9eddcf5e2306da9098232d5db24237ccd1616
Jarryd Beck <jarro.2783@gmail.com>
2016-08-31 08:48:22 +1000

    lint main

--- site/main.py
+++ site/main.py
@@ -1,5 +1,8 @@
+"""
+This is the main AusNethack web module.
+"""
+
 from flask import Flask, render_template
-import flask
 import sqlite3
 import wwwnethack as wwwnh
 
@@ -8,37 +11,45 @@ app.config.from_object('wwwconfig')
 #app.config.servername = 'AusNethack'
 
 def sql_connect(game):
+    """ Connect to the nethack sqlite database."""
+
     conn = sqlite3.connect(app.config['NETHACKDB'][game])
     conn.row_factory = sqlite3.Row
     return conn
 
 def sql_query(game, query):
+    """ Run a query on the database."""
+
     conn = sql_connect(game)
     cursor = conn.cursor()
     return cursor.execute(query).fetchall()
 
 @app.route('/')
 def main():
+    """Index page."""
     #app.config.update(dict(title='nethack'))
     return render_template('index.html')
 
 @app.route('/users')
 def users():
+    """Users page"""
+
     sql = sql_connect('360')
-    c = sql.cursor()
-    games = c.execute("""
+    cursor = sql.cursor()
+    games = cursor.execute("""
       SELECT SUM(sessions.end_time - sessions.start_time) AS total,
         plname,
         COUNT(DISTINCT games.id) AS numgames
       FROM games JOIN sessions ON games.id = sessions.game GROUP BY plname
-    """
-    ).fetchall()
+    """).fetchall()
 
     app.config.pagename = 'Users'
-    return render_template('users.html', users = games)
+    return render_template('users.html', users=games)
 
 @app.route('/zscores')
 def zscores():
+    """Zscores page."""
+
     conn = sql_connect('360')
     cursor = conn.cursor()
     rows = cursor.execute("""
@@ -54,22 +65,22 @@ def zscores():
     for player in sorted(scores.keys(), reverse=True):
         score_list.append(wwwnh.ZScore(player, scores[player]))
 
-    return render_template('zscores.html', scores = score_list, roles = roles)
+    return render_template('zscores.html', scores=score_list, roles=roles)
 
 @app.route('/high_scores')
 def high_scores():
-    scores = sql_query('360',
-      """
-      SELECT games.*,
-        sum(sessions.end_time - sessions.start_time) AS total_time
-      FROM games JOIN sessions ON sessions.game = games.id
-      WHERE score NOT NULL
-      GROUP BY games.id
-      ORDER BY score DESC LIMIT 2000
-      """
-    )
+    """High scores page."""
 
-    return render_template('high_scores.html', scores = scores)
+    scores = sql_query('360', """
+        SELECT games.*,
+            sum(sessions.end_time - sessions.start_time) AS total_time
+        FROM games JOIN sessions ON sessions.game = games.id
+        WHERE score NOT NULL
+        GROUP BY games.id
+        ORDER BY score DESC LIMIT 2000
+        """)
+
+    return render_template('high_scores.html', scores=scores)
 
 if __name__ == '__main__':
     app.run(debug=True)
