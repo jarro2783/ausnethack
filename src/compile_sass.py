8394708c94de61616ee071899f4e138b508b7172
Jarryd Beck <jarro.2783@gmail.com>
2016-09-02 08:31:46 +1000

    build css

--- src/compile_sass.py
+++ src/compile_sass.py
@@ -7,35 +7,54 @@ import sass
 
 def main():
     parser = argparse.ArgumentParser(description='Compile sass')
-    parser.add_argument('--srcdir', dest='sourcedir')
-    parser.add_argument('--dstdir', dest='destdir')
+    parser.add_argument('--input')
+    parser.add_argument('--output')
     parser.add_argument('--minify',
         default=False, help='Minify the output', action='store_const',
         const=True)
+    parser.add_argument('--deps', help='Write dependencies to file')
 
     args = parser.parse_args()
 
-    if args.sourcedir is None:
-        args_error('sourcedir')
+    if args.input is None:
+        args_error('input')
 
-    if args.destdir is None:
-        args_error('destdir')
+    if args.output is None:
+        args_error('output')
 
-    compile(args.sourcedir, args.destdir, args.minify)
+    compile(args.input, args.output, args.minify, args.deps)
 
 def args_error(arg):
     print('{} must be given on the command line'.format(arg),
         file=sys.stderr)
     sys.exit(1)
 
-def compile(source, dest, minify):
+def gather_deps(deps, src):
+    def add_dep(path):
+        deps.append("css/_{}.scss".format(path))
+        return None
+    return add_dep
+
+def compile(source, dest, minify, deps):
 
     kwargs = {}
 
     if minify:
         kwargs['output_style'] = 'compressed'
 
-    sass.compile(dirname=(source, dest), **kwargs)
+    if deps is not None:
+        dependencies = []
+        kwargs['importers'] = [(0, gather_deps(dependencies, source))]
+
+    result = sass.compile(filename=source,
+        **kwargs)
+
+    output = open(dest, 'w')
+    output.write(result)
+
+    if deps is not None:
+        output = open(deps, 'w')
+        output.write("{}: {}\n".format(dest, ' '.join(dependencies)))
 
 if __name__ == '__main__':
     main()
