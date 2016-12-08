7777d5120c3c643f391420ccc990aa1188ee87f4
Jarryd Beck <jarro.2783@gmail.com>
2016-12-09 07:11:19 +1100

    set default recordings backend to s3
    There was no default set previously.

--- site/wwwconfig.py
+++ site/wwwconfig.py
@@ -7,6 +7,7 @@ EMAIL = "jarro.2783@gmail.com"
 SITE_GITHUB = github + "/ausnethack"
 LAUNCHER_GITHUB = github + "/pygamelaunch"
 NETHACK_GITHUB = github + "/nethack"
+RECORDINGS_BACKEND = 's3'
 S3_RECORDINGS_CONFIG = {
     'bucket' : 'ausnethack-recordings-compressed',
 }
